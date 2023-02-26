8aa4d913085641af5f23532445025b31d818e6d9
Neila Amandolla <91745858+Nellllyaa@users.noreply.github.com>
2023-02-26 13:51:39 +0600

    Update main.go
    Added authorization function

--- go-reg/main.go
+++ go-reg/main.go
@@ -50,8 +50,60 @@ func registrationHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func authorizationHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles("registration.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else if r.Method == "POST" {
+		// Get the form data from the request
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// Check the user's credentials against the database or other authentication service
+		if username == "admin" && password == "password123" {
+			// Authenticx`x	ation successful, redirect the user to the dashboard
+			http.Redirect(w, r, "/dashboard", http.StatusFound)
+		} else {
+			// Authentication failed, render an error page
+			tmpl, err := template.ParseFiles("authorization-error.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			err = tmpl.Execute(w, nil)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("dashboard.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
 	http.HandleFunc("/register", registrationHandler)
+	http.HandleFunc("/authorize", authorizationHandler)
+	http.HandleFunc("/dashboard", dashboardHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	log.Println("Server is listening on port 8080...")
 	err := http.ListenAndServe(":8080", nil)
